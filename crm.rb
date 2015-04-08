a41372794c41d9dea343a5418170c6fcb635e810
Warren Phen <warren.phen@gmail.com>
2015-04-08 15:07:34 -0400

    created route view for new_contact

--- crm.rb
+++ crm.rb
@@ -2,19 +2,20 @@ require_relative 'rolodex'
 require_relative 'contact'
 require 'sinatra'
 
+$rolodex= Rolodex.new
+
+#routes
 get '/' do
   @crm_app_name = "My CRM"
   erb :index
 end
 
 get '/contacts' do
-  
-  @contacts = []
-  @contacts << Contact.new("Yehuda", "Katz", "yehuda@example.com", "Developer")
-  @contacts << Contact.new("Mark", "Zuckerberg", "mark@facebook.com", "CEO")
-  @contacts << Contact.new("Sergey", "Brin", "sergey@google.com", "Co-Founder")
-	
-  erb :contacts
+  	erb :contacts
+end
+
+get '/contacts/new' do
+	erb :new_contact
 end
 
 
--- rolodex.rb
+++ rolodex.rb
@@ -1,34 +1,14 @@
 class Rolodex
+  attr_reader :contacts
 
-	# attr_reader :contacts
+  def initialize
+    @contacts = []
+    @id = 1000
+  end
 
-	def initialize
-		@contacts = []
-		@next_contact_id = 1
-	end
-
-	def add_contact(contact)
-		contact.id = @next_contact_id
-		@contacts << contact
-		@next_contact_id += 1
-	end
-
-	def find(contact_id)
-		@contacts.find do |contact|
-			contact.id == contact_id
-		end
-	end
-
-	def delete(contact_id)
-		@contacts.delete_if do |contact|
-			contact.id == contact_id
-		end
-	end
-
-	def each_contact
-		@contacts.each do |contact|
-			yield contact
-		end
-	end
-
+  def add_contact(contact)
+    contact.id = @id
+    @contacts << contact
+    @id += 1
+  end
 end
