e819bc421a5ae21b255f46ac797c74bc4946046f
Warren Phen <warren.phen@gmail.com>
2015-04-10 15:24:22 -0400

    created route to handle put request

--- crm.rb
+++ crm.rb
@@ -39,7 +39,12 @@ end
 get "/contacts/:id/edit" do
   @contact = $rolodex.find(params[:id].to_i)
   if @contact
-    erb :edit_contact
+  	@contact.first_name = params[:first_name]
+    @contact.last_name = params[:last_name]
+    @contact.email = params[:email]
+    @contact.note = params[:note]
+    
+    redirect to("/contacts")
   else
     raise Sinatra::NotFound
   end
