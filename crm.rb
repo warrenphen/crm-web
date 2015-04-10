1f7db7e288ec8df1119ec7b3665ce224a3ea74ec
Warren Phen <warren.phen@gmail.com>
2015-04-10 15:19:19 -0400

    added ability to edit contacts

--- crm.rb
+++ crm.rb
@@ -35,3 +35,13 @@ get "/contacts/:id" do
     raise Sinatra::NotFound
   end
 end
+
+get "/contacts/:id/edit" do
+  @contact = $rolodex.find(params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
