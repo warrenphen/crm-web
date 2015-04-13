6d62cf12c2507a61b4719bccb74df99ced952411
Warren Phen <warren.phen@gmail.com>
2015-04-13 14:13:57 -0400

    deleted rolodex.rb, updated methods in crm.rb

--- crm.rb
+++ crm.rb
@@ -1,8 +1,6 @@
-require_relative 'rolodex'
 require 'sinatra'
 require 'data_mapper'
 
-$rolodex = Rolodex.new
 
 DataMapper.setup(:default, "sqlite3:database.sqlite3")
 
@@ -36,7 +34,7 @@ end
 
 
 get "/contacts/:id" do
-  @contact = $rolodex.find(params[:id].to_i)
+  @contact = Contact.get(params[:id].to_i)
   if @contact
     erb :show_contact
   else
@@ -45,7 +43,7 @@ get "/contacts/:id" do
 end
 
 get "/contacts/:id/edit" do
-  @contact = $rolodex.find( params[ :id ].to_i )
+  @contact = Contact.get( params[ :id ].to_i )
   if @contact
     erb :edit_contact
   else
@@ -54,7 +52,7 @@ get "/contacts/:id/edit" do
 end
 
 put "/contacts/:id" do
-  @contact = $rolodex.find(params[:id].to_i)
+  @contact = Contact.get(params[:id].to_i)
   if @contact
   	@contact.first_name = params[:first_name]
     @contact.last_name = params[:last_name]
@@ -68,19 +66,23 @@ put "/contacts/:id" do
 end
 
  delete "/contacts/:id" do
-  @contact = $rolodex.find(params[:id].to_i)
+  @contact = Contact.get(params[:id].to_i)
   if @contact
-    $rolodex.remove_contact(@contact)
+    @contact.destroy
     redirect to("/contacts")
   else
     raise Sinatra::NotFound
   end
 end
 
-post '/contacts' do
-	new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
-	$rolodex.add_contact(new_contact)
-	redirect to('/contacts')
+post "/contacts" do
+ contact = Contact.create(
+    :first_name => params[:first_name],
+    :last_name => params[:last_name],
+    :email => params[:email],
+    :note => params[:note]
+  )
+  redirect to('/contacts')
 end
 
 
